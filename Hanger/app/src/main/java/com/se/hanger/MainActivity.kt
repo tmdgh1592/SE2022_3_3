518b2e21693e9efaad366a72211f599af9523aaf
tmdgh1592 <tmdgh1592@naver.com>
2022-05-17 21:17:13 +0900

    로그인 액티비티 생성

--- Hanger/app/src/main/java/com/se/hanger/MainActivity.kt
+++ Hanger/app/src/main/java/com/se/hanger/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.se.hanger
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.se.hanger.databinding.ActivityMainBinding
 
