757891d30bef9b0948d06d156a1aacd40105f8de
tmdgh1592 <tmdgh1592@naver.com>
2022-05-25 20:25:52 +0900

    feat: Member data class 생성 related to #7

---
+++ Hanger/app/src/main/java/com/se/hanger/data/model/Member.kt
@@ -0,0 +1,12 @@
+package com.se.hanger.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Member(
+    @SerializedName("id") val id: String,
+    @SerializedName("password") var password: String,
+    @SerializedName("email") val email: String,
+    @SerializedName("username") val username: String,
+    @SerializedName("loginStatus") val loginStatus: Boolean,
+    @SerializedName("loginTrial") val loginTrial: Int,
+)
