2ac140a8e261de80a6b55b93c07f867db0f03aca
tmdgh1592 <tmdgh1592@naver.com>
2022-05-31 21:41:03 +0900

    feat: 캘린더 라이브러리 추가 및 기본 화면 구현 related to: #24

--- Hanger/app/src/main/java/com/se/hanger/view/main/CalendarFragment.kt
+++ Hanger/app/src/main/java/com/se/hanger/view/main/CalendarFragment.kt
@@ -1,19 +1,22 @@
 package com.se.hanger.view.main
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.se.hanger.R
+import androidx.fragment.app.Fragment
+import com.se.hanger.databinding.FragmentCalendarBinding
 
 class CalendarFragment : Fragment() {
+
+    private lateinit var binding: FragmentCalendarBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_calendar, container, false)
+        binding = FragmentCalendarBinding.inflate(inflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
