c1c17c90db7dbacf13c866387389c77e33096b25
kms146594 <90440349+kms146594@users.noreply.github.com>
2022-06-01 23:56:16 +0900

    model_1

---
+++ Model.java
@@ -0,0 +1,18 @@
+public class Model {
+    private String imageUrl;
+
+    Model(){
+    }
+
+    public Model(String imageUrl){
+        this.imageUrl = imageUrl;
+    }
+
+    public String getImageUrl(){
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl){
+        this.imageUrl = imageUrl;
+    }
+}
