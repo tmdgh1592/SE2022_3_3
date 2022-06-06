d8a09e3ee5a38034b15f8cbc1a7364ee4ccaeff6
wooseok9872 <9872dan@naver.com>
2022-06-06 17:59:32 +0900

    cloth crud

---
+++ Hanger/app/src/main/java/com/se/hanger/view/adapter/TagAdapter.kt
@@ -0,0 +1,48 @@
+package com.se.hanger.view.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.se.hanger.R
+
+class TagAdapter(val dataSet: ArrayList<String>) :
+    RecyclerView.Adapter<TagAdapter.ViewHolder>() {
+
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder).
+     */
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView
+
+        init {
+            // Define click listener for the ViewHolder's View.
+            textView = view.findViewById(R.id.tag_view)
+        }
+    }
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        // Create a new view, which defines the UI of the list item
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.tag_row_item, viewGroup, false)
+
+        return ViewHolder(view)
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        viewHolder.textView.text = dataSet[position]
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = dataSet.size
+
+
+
+}
--- Hanger/app/src/main/java/com/se/hanger/view/cloth/ClothAddDialogFragment.kt
+++ Hanger/app/src/main/java/com/se/hanger/view/cloth/ClothAddDialogFragment.kt
@@ -1,32 +1,55 @@
 package com.se.hanger.view.cloth
 
+import android.app.Activity
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.*
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.DialogFragment
+import com.bumptech.glide.Glide
+import com.github.drjacky.imagepicker.ImagePicker
+import com.google.gson.Gson
 import com.se.hanger.R
+import com.se.hanger.data.db.ClothDatabase
+import com.se.hanger.data.model.*
 import com.se.hanger.databinding.FragmentDialogClothAddBinding
 import com.se.hanger.utils.ScreenSizeProvider
+import com.se.hanger.view.adapter.TagAdapter
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import okhttp3.internal.notify
+import org.threeten.bp.LocalDate
 
 class ClothAddDialogFragment : DialogFragment(), View.OnClickListener {
     private lateinit var binding: FragmentDialogClothAddBinding
+    private lateinit var clothDB: ClothDatabase
+    private lateinit var date: LocalDate
+    private lateinit var adapter: TagAdapter
+    private var photo: Photo? = null
+    private var galleryLauncher: ActivityResultLauncher<Intent>? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentDialogClothAddBinding.inflate(inflater)
+        clothDB = ClothDatabase.getInstance(requireContext())!!
         dialog?.window?.requestFeature(Window.FEATURE_NO_TITLE)
+        date = Gson().fromJson(arguments?.getString("date"), LocalDate::class.java)
+        adapter = TagAdapter(ArrayList())
+        binding.tagRecyclerView.adapter = adapter
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        with(binding) {
-            setClickListener()
-        }
+        setLauncher()
+        setClickListener()
     }
 
     override fun onResume() {
@@ -46,7 +69,46 @@ class ClothAddDialogFragment : DialogFragment(), View.OnClickListener {
     }
 
     private fun setClickListener() {
-        binding.closeBtn.setOnClickListener(this)
+        with(binding) {
+            /* 태그 추가 로직 */
+            tagAddBtn.setOnClickListener {
+                adapter.dataSet.add(tagAddEt.text.toString())
+                adapter.notify()
+            }
+
+            /* 메인 사진 추가 로직 */
+            clothImgAddBtn.setOnClickListener {
+                // 갤러리 실행
+                galleryLauncher?.launch(
+                    ImagePicker.with(requireActivity())
+                        .galleryOnly()
+                        .createIntent()
+                )
+            }
+
+            /* 추가 버튼 이벤트 설정*/
+            clothAddBtn.setOnClickListener {
+
+                // DB 는 IO 작업이기 때문에 scope 열어줌
+                // TODO 각종 ui 에서 데이터 가져와서 설정해주자
+                CoroutineScope(Dispatchers.IO).launch {
+                    val cloth = Cloth(
+                        buyUrl = buyerEt.text.toString(),
+                        clothSize = "",
+                        clothName = "",
+                        clothMemo = "",
+                        clothPhoto = "",
+                        dailyPhoto = listOf(photo!!),
+                        tags = adapter.dataSet.map { data ->
+                            Tag("", data)
+                        },
+                        categories = listOf(Category(Season.SPRING, CategoryCloth.ACCESSORY))
+                    )
+                    clothDB.clothDao().insert(cloth)
+                }
+            }
+
+        }
     }
 
     override fun onClick(view: View?) {
@@ -55,5 +117,21 @@ class ClothAddDialogFragment : DialogFragment(), View.OnClickListener {
                 dismiss()
             }
         }
+    }
+
+    private fun setLauncher() {
+        galleryLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                if (it.resultCode == Activity.RESULT_OK) {
+                    // Use the uri to load the image
+                    val uri = it.data?.data!!
+                    // 갤러리에서 불러온 이미지 적용
+                    Glide.with(requireContext()).load(uri).into(binding.photoIv)
+                    photo =
+                        Photo(
+                            System.currentTimeMillis().toString(), uri.toString(),
+                        )
+                }
+            }
     }
 }
